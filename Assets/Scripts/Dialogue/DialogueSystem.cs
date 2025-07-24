4400ce2f63fb0d9beb14b5ff854ffebad1419bf5
Modin <eom3658@gmail.com>
2025-07-24 16:58:08 +0900

    feat: DialogueUI

--- Assets/Scripts/Dialogue/DialogueSystem.cs
+++ Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -5,5 +5,21 @@ namespace Modin
     public class DialogueSystem : MonoBehaviour
     {
         [SerializeField] private DialogueDB dialogueDB;
+        [SerializeField] private DialogueUI dialogueUI;
+        
+        private DialogueChapter currentChapter;
+        private DialogueSequence currentSequence;
+        private DialogueLine currentLine;
+        
+        private void ShowCurrentLine()
+        {
+            // UI 관리 객체
+            // UI 관리 객체 없이. System~ 직접 관리한다.
+        }
+        
+        private void NextLine()
+        {
+            
+        }
     }
 }
